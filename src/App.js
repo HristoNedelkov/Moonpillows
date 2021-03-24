e92cec5ea763cda5f29538a9a96429843d8d2b03
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-03-24 15:22:12 +0200

    More fixes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
