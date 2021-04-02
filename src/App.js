77f809c1c06afc93bec33b33ff56e895a797b8b3
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-04-02 17:50:30 +0300

    Fixed domain

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/Moonpillows' exact ><Redirect to="/"/></Route>
+          <Route path='/Moonpillows' exact component={Home}></Route>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
