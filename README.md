ed08293b103e844a86e1b2a6e5cc1e1f2ba870c2
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-04-02 17:45:50 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Moonpillows
-
-https://hristonedelkov.github.io/
---
+++ precache-manifest.32693e7cb3896083128a2c68d8378f70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4aff3c86b8706e8a0a32a9ed28049b8",
+    "url": "/Moonpillows/index.html"
+  },
+  {
+    "revision": "11ce5c4dae9f66bac076",
+    "url": "/Moonpillows/static/css/main.69aa224b.chunk.css"
+  },
+  {
+    "revision": "e63873fcfe7fffa6e6c5",
+    "url": "/Moonpillows/static/js/2.ea46acf3.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Moonpillows/static/js/2.ea46acf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a9c82eb8e1262c4df9",
+    "url": "/Moonpillows/static/js/3.1507f391.chunk.js"
+  },
+  {
+    "revision": "11ce5c4dae9f66bac076",
+    "url": "/Moonpillows/static/js/main.bfaec3b2.chunk.js"
+  },
+  {
+    "revision": "0c403c3908148b346019",
+    "url": "/Moonpillows/static/js/runtime-main.86c5a1b7.js"
+  }
+]);
