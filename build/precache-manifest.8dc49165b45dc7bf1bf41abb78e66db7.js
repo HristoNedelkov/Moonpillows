6b6c252fa81e472b4157527258fffa012081c573
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-04-02 09:50:14 +0300

    Deploy2

---
+++ build/precache-manifest.8dc49165b45dc7bf1bf41abb78e66db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78eb0f9ec28850c6db61cd3fffc2673b",
+    "url": "/Moonpillows/index.html"
+  },
+  {
+    "revision": "20b2fc2cbdefdfe84595",
+    "url": "/Moonpillows/static/css/main.69aa224b.chunk.css"
+  },
+  {
+    "revision": "3ab0ab2a7ee99ebefb4c",
+    "url": "/Moonpillows/static/js/2.7ab6cdf1.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Moonpillows/static/js/2.7ab6cdf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89092f98c371e60a6a64",
+    "url": "/Moonpillows/static/js/3.78a7a0de.chunk.js"
+  },
+  {
+    "revision": "20b2fc2cbdefdfe84595",
+    "url": "/Moonpillows/static/js/main.99eb71c3.chunk.js"
+  },
+  {
+    "revision": "6e270645d058c4edbb51",
+    "url": "/Moonpillows/static/js/runtime-main.6afa44ae.js"
+  }
+]);
