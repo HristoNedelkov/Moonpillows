5b5f5bafb6b422a158809e623cf78046c56cec5b
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-04-02 09:47:40 +0300

    adding hosted video

---
+++ build/precache-manifest.697f88c3d5885a8965c36f097fa651a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f998b96b225b05dd7e4ba22f0ff771",
+    "url": "/Moonpillows/index.html"
+  },
+  {
+    "revision": "792c40e95bb05c465ef1",
+    "url": "/Moonpillows/static/css/main.69aa224b.chunk.css"
+  },
+  {
+    "revision": "3ab0ab2a7ee99ebefb4c",
+    "url": "/Moonpillows/static/js/2.7ab6cdf1.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Moonpillows/static/js/2.7ab6cdf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308e42ba462f7ac98871",
+    "url": "/Moonpillows/static/js/3.f6a97685.chunk.js"
+  },
+  {
+    "revision": "792c40e95bb05c465ef1",
+    "url": "/Moonpillows/static/js/main.2d81efb4.chunk.js"
+  },
+  {
+    "revision": "2990a2536b47405b2845",
+    "url": "/Moonpillows/static/js/runtime-main.30071508.js"
+  }
+]);
--- src/components/VideoComp.js
+++ src/components/VideoComp.js
@@ -3,7 +3,7 @@ import React from 'react'
 function VideoComp() {
 
     return (
-        <video src='https://drive.google.com/file/d/1d1UJOVu8dNEatwi7g8JoHCG-LdbbFy93/view' autoPlay loop muted   />
+        <video src='https://drive.google.com/file/d/1d1UJOVu8dNEatwi7g8JoHCG-LdbbFy93/preview' autoPlay loop muted   />
     )
 }
 
