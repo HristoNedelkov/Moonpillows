857d76dcb7f745a68977c24c0698a3ff7ebb3b6a
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2021-04-05 11:38:12 +0300

    Updates

---
+++ precache-manifest.2e56274ceb7818d3bc8e1c5358a55ab2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03afe102d871818ead20fa437b0fe59",
+    "url": "/Moonpillows/index.html"
+  },
+  {
+    "revision": "23053e630e11b24bde27",
+    "url": "/Moonpillows/static/css/3.df8e1aac.chunk.css"
+  },
+  {
+    "revision": "46178ae8b0921f454bfd",
+    "url": "/Moonpillows/static/css/main.8efef589.chunk.css"
+  },
+  {
+    "revision": "c15c7c58eb57d5c335a1",
+    "url": "/Moonpillows/static/js/2.f90a02fa.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Moonpillows/static/js/2.f90a02fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23053e630e11b24bde27",
+    "url": "/Moonpillows/static/js/3.8bb1b9d9.chunk.js"
+  },
+  {
+    "revision": "46178ae8b0921f454bfd",
+    "url": "/Moonpillows/static/js/main.c07f5feb.chunk.js"
+  },
+  {
+    "revision": "84914d27eaa42d9ddd24",
+    "url": "/Moonpillows/static/js/runtime-main.a451b9e8.js"
+  }
+]);
